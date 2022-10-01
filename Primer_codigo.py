45e131e803887f441665f3afcc76af146a0ce3e2
fabianrobayo11 <114267586+fabianrobayo11@users.noreply.github.com>
2022-09-30 21:17:47 -0500

    Add files via upload

---
+++ Primer_codigo.py
@@ -0,0 +1,11 @@
+print("hola Big Data!!!")
+frase = "hola Big Data!!!"
+print (frase)
+frase = "hola Big Data!!!"
+print(frase)
+
+
+import numpy as np
+import matplotlib.pyplot as plt #para graficar
+import scipy
+import cvxopt
